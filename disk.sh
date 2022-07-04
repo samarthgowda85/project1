6da655a2392b92acc6f57ac07e1be847fae47d75
samarthgowda85 <107824461+samarthgowda85@users.noreply.github.com>
2022-07-04 21:58:05 +0530

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 disk=`df -h . | awk -F " " 'NR==2 {print $5}' | sed 's/%/ /g'`
 
-sat   if [ $disk -ge 30 ]
+if [ $disk -ge 30 ]
 then
 	echo "memory reached thershold value" | mail -s "memory utilization" samarthgowda85@gmail.com
 fi
